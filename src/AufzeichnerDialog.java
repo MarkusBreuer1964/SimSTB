df64d7d19664bb56896c9dd4da543d96d200ff98
Markus Breuer <markus.breuer@berufskolleg-aachen.de>
2019-07-24 17:06:17 +0200

    Datenaufzeichnung eingefügt

--- src/AufzeichnerDialog.java
+++ src/AufzeichnerDialog.java
@@ -28,10 +28,11 @@ public class AufzeichnerDialog extends JDialog implements ActionListener {
 	public AufzeichnerDialog() {
 		setTitle("SimSTB - Datenaufzeichnung");
 		setIconImage(Toolkit.getDefaultToolkit().getImage("Z:\\Sonstiges\\Markus\\Weiterbildung\\Java\\workspace\\SimSTB\\bilder\\settings-gears.png"));
-		setBounds(100, 100, 679, 142);
-		getContentPane().setLayout(new BorderLayout());
+		setBounds(100, 100, 583, 158);
+		getContentPane().setLayout(null);
+		contentPanel.setBounds(0, 0, 567, 75);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		getContentPane().add(contentPanel);
 		contentPanel.setLayout(null);
 		
 		JLabel lblIntervals = new JLabel("Interval (s)");
@@ -40,16 +41,16 @@ public class AufzeichnerDialog extends JDialog implements ActionListener {
 		
 		textField = new JTextField();
 		textField.setText("5");
-		textField.setBounds(79, 8, 46, 20);
+		textField.setBounds(74, 8, 30, 20);
 		contentPanel.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblLogdatei = new JLabel("Log-Datei");
-		lblLogdatei.setBounds(10, 36, 46, 14);
+		lblLogdatei.setBounds(10, 36, 62, 14);
 		contentPanel.add(lblLogdatei);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(79, 33, 383, 20);
+		textField_1.setBounds(74, 33, 388, 20);
 		contentPanel.add(textField_1);
 		textField_1.setColumns(10);
 		
@@ -58,17 +59,20 @@ public class AufzeichnerDialog extends JDialog implements ActionListener {
 		contentPanel.add(btnDatei);
 		{
 			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			buttonPane.setBounds(0, 80, 567, 33);
+			getContentPane().add(buttonPane);
 			{
 				okButton = new JButton("Datenaufzeichnung starten");
+				okButton.setBounds(253, 5, 209, 23);
 				okButton.addActionListener( this);
+				buttonPane.setLayout(null);
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
 				cancelButton = new JButton("Cancel");
+				cancelButton.setBounds(472, 5, 89, 23);
 				cancelButton.addActionListener( this);
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
