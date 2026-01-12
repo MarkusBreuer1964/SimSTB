baf3f052ae2bcd7e376f7ac7806a5add7d593e5d
Markus Breuer <markus.breuer@berufskolleg-aachen.de>
2026-01-12 14:21:02 +0100

    Sicherung 12.1.2026

---
+++ Webdokumentation/simstb_dokumentation.js
@@ -0,0 +1,13 @@
+/* simstb_dokumentation.js - Dokumentation SimSTB - Javascript-Datei
+    Name, Organisaion:          Markus Breuer, BK-GuT
+    Erstellt, Letzte Änderung:  07.02.2025, 18.02.2025
+-->
+*/
+
+    function w3_open() {
+        document.getElementById("mySidebar").style.display = "block";
+    }
+
+    function w3_close() {
+        document.getElementById("mySidebar").style.display = "none";
+    }
